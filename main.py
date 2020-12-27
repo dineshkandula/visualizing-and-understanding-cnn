4c211793edd09645fee137e390e72d494a73daac
Dinesh Kandula <dineshkandulakkr@gmail.com>
2020-12-27 21:09:35 +0530

    Adding global definitions for visualization params
    Adding seperate global definitions for the following parameters needed for visualization
    so that they can be modified easily.
    1. TEST_IMAGE_PATH (path to image)
    2. VISUALIZE_LAYER (name of the layer)
    3. VISUALIZE_FEATURE (feature number)
    4. VISUALIZE_MODE ('all' or 'max')
    
    Signed-off-by: Dinesh Kandula <dineshkandulakkr@gmail.com>

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ from visualize_cnn import visualize
 
 TEST_IMAGE_PATH = 'test_images/car.jpeg'
 
+VISUALIZE_LAYER = 'block3_conv3'
+VISUALIZE_FEATURE = 127
+VISUALIZE_MODE='all' #or 'max'
+
 vgg_model = vgg16.VGG16(weights='imagenet', include_top = True)
 
 img = load_img(TEST_IMAGE_PATH, target_size=(224, 224), interpolation='nearest')
@@ -24,4 +28,4 @@ img = vgg16.preprocess_input(image_batch)
 plt.imshow(img[0])
 plt.show()
 
-visualize(img, vgg_model, 'block3_conv3', 127, 'all')
+visualize(img, vgg_model, VISUALIZE_LAYER, VISUALIZE_FEATURE, VISUALIZE_MODE)
